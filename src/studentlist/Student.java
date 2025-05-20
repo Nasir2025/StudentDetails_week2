fb8b5b4616e9abdd80501e488c8052dd2a064ef6
Nasir2025 <hamedy@sheridancollege.ca>
2025-05-20 15:39:47 -0400

    Update Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -12,6 +12,7 @@ public class Student // model the student raw data
 {
     private String name; // variables private 
    private int sid; // principle "encapsulation"
+    private String last;
 private int sem;
     /**
      * @return the name
