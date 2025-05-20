20b1235da46bd068e10371310218c1d8f762e130
Nasir2025 <hamedy@sheridancollege.ca>
2025-05-20 15:49:30 -0400

    Update Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package studentlist;
-
+/** May 20th changes
 /** entity 
  *
  * @author sivagamasrinivasan
